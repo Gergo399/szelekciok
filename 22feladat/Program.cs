57f8c247df21f68d3bd7072fe1a7bf7d1abd563c
tanulo <gergokovcs.b@gmail.com>
2026-09-24 10:55:15 +0200

    23.feladat kész

---
+++ 22feladat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot: ");
+            int szam1= int.Parse(Console.ReadLine());
+            if (szam1%3==0)
+            {
+                Console.WriteLine("A szám osztható 3-al");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható hárommal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
